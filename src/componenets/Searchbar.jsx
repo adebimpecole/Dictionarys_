5cbb437bba680c93d2aeb42474a0fafd0edbbfba
Mary <adebimpecole02@gmail.com>
2024-03-28 11:02:06 +0100

    added responsiveness

--- src/componenets/Searchbar.jsx
+++ src/componenets/Searchbar.jsx
@@ -1,32 +1,58 @@
-import React, { useState, useContext, useEffect } from 'react'
-import Context from '../Context'
-import srch from "../assets/search.svg"
-import "./Searchbar.sass"
+import React, { useState, useContext, useEffect } from "react";
+import Context from "../Context";
+import srch from "../assets/search.svg";
+import "./Searchbar.sass";
 
 const Searchbar = () => {
-    const  [val, setVal] = useState('')
-    const  [stat, setStat] = useState(false)
-    const state = useContext(Context);
-    const inputbox = document.querySelector("#gsearch");
+  const [val, setVal] = useState("");
+  const [stat, setStat] = useState(false);
+  const word = useContext(Context);
+  const inputbox = document.querySelector("#gsearch");
 
-    useEffect(()=>{
-        if(inputbox){
-            setVal(inputbox.value)
-            console.log(val)
-            state(inputbox.value);
-        }
-        setStat(false)
-    },[stat, setStat]) 
+  useEffect(() => {
+    if (inputbox) {
+      setVal(inputbox.value);
+      word(inputbox.value);
+    }
+    setStat(false);
+  }, [stat, setStat]);
+
+  useEffect(() => {
+    setVal("");
+  }, [word]);
+
+  const handleKeyDown = (event) => {
+    if (event.key === "Enter") {
+      setStat(true);
+    }
+  };
 
   return (
-    <div className='search-box'>
-        <form className='search_box'>
-            <label htmlFor="gsearch"><img src={srch} className="search_icon" alt="icon" onClick={()=>(setStat(true))}/></label>
-            {/* <label htmlFor="gsearch"><img src={srch} className="search_icon" alt="icon"/></label> */}
-            <input defaultValue={val} type="search" id="gsearch" name="gsearch" />
-        </form>
+    <div className="search-box">
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+        }}
+        className="search_box"
+      >
+        <label htmlFor="gsearch">
+          <img
+            src={srch}
+            className="search_icon"
+            alt="icon"
+            onClick={() => setStat(true)}
+          />
+        </label>
+        <input
+          defaultValue={val}
+          type="search"
+          id="gsearch"
+          name="gsearch"
+          onKeyDown={handleKeyDown}
+        />
+      </form>
     </div>
-  )
-}
+  );
+};
 
-export default Searchbar
+export default Searchbar;
